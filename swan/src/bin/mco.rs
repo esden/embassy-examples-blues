b141561f4730631599b07508a04bee50d0b37d5d
Piotr Esden-Tempski <piotr@esden.net>
2025-03-26 11:13:44 -0700

    Initial commit with the swan board examples.

---
+++ swan/src/bin/mco.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::rcc::{Mco, McoPrescaler, McoSource};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+
+    // NOTE: the MCO pin is not routed to any pins on the Swan board
+    let _mco = Mco::new(p.MCO, p.PA8, McoSource::HSI, McoPrescaler::DIV1);
+
+    let mut led = Output::new(p.PE2, Level::High, Speed::Low);
+
+    loop {
+        led.set_high();
+        Timer::after_millis(300).await;
+        led.set_low();
+        Timer::after_millis(300).await;
+    }
+}
